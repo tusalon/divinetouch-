92f9918ff33886e453560602422fd42bfb1f7ef9
TuDespensa25 <roma220130@gmail.com>
2026-06-15 14:02:28 -0400

    Actualizar logica de reservas y APK

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // sw.js - Service Worker para Divine Touch
 
-const CACHE_NAME = 'divinetouch--v50';
+const CACHE_NAME = 'divinetouch--v56';
 const urlsToCache = [
   '/divinetouch-/',
   '/divinetouch-/index.html',
